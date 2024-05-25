09db79c37282bc27b3c4744e54d7b490612e9faf
Alec Flett <alecf@flett.org>
2024-05-24 18:02:49 -0700

    update simplestream example

--- examples/simplestream.ts
+++ examples/simplestream.ts
@@ -1,18 +1,19 @@
 import crypto from "crypto";
 import { f, objectTemplate } from "../src";
-import { OpenAI } from "../src/client";
+import { Anthropic } from "../src/client";
 
 async function main() {
-  const openai = new OpenAI({
+  const anthropic = new Anthropic({
     // apiKey: process.env.OPENAI_API_KEY
   });
 
   console.log("Testing Streaming Chat API...");
-  const completion = await openai.chat.completions.create({
+  const completion = await anthropic.messages.create({
     messages: objectTemplate([
       { role: "user", content: "Tell a 20 word story about {name}" },
     ]) as any,
-    model: "gpt-3.5-turbo",
+    model: "claude-3-haiku-20240307",
+    max_tokens: 1024,
     stream: true,
     libretto: {
       promptTemplateName: "ts-client-test-chat",
@@ -20,14 +21,16 @@ async function main() {
       feedbackKey: crypto.randomUUID(),
     },
   });
+  console.log("got completion: ", completion);
   for await (const result of completion) {
-    console.log("Streamed Chat API replied with: ", result.choices);
+    console.log("Streamed Chat API replied with: ", result);
   }
   console.log("Testing Streaming Completion API...");
-  const completion2 = await openai.completions.create({
+  const completion2 = await anthropic.completions.create({
     prompt: f`Tell a 20 word story about {name}` as unknown as string,
     model: "davinci",
     stream: true,
+    max_tokens_to_sample: 1024,
     libretto: {
       promptTemplateName: "ts-client-test-chat",
       templateParams: { name: "John" },
